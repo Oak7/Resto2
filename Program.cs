1a5110da60abd939d5a73464b5da83b6b2d0e803
Gabriela <aragao.gabi@gmail.com>
2021-09-04 23:32:22 -0300

    mostra os valores do resto de 2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite n: ");
+            int n = int.Parse(Console.ReadLine());
+            int i;
+            
+            for (i=1;i<=10000;i++){        
+                if (i%n==2){                    
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
